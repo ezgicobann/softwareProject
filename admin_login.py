181968e5c82be06f66fae5edff37abf5992716b9
Aylin İrem Acar <ayliniremacar0@gmail.com>
2024-12-19 17:33:31 +0300

    Admin Login Page

---
+++ admin_login.py
@@ -0,0 +1,92 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from voiceshow import VoiceShow  # VoiceShow sınıfını import ettik
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(426, 217)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(0, -1, -1, -1)
+        self.gridLayout.setHorizontalSpacing(7)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setEnabled(True)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 2, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.gridLayout.addLayout(self.verticalLayout, 1, 1, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 5, 1, 1, 1)
+        self.horizontalLayout.addLayout(self.gridLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 426, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Admin Login"))
+        self.label.setText(_translate("MainWindow", "Şifre:"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600;\">Admin Girişi</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Giriş"))
+
+class AdminLogin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(AdminLogin, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Giriş butonuna tıklama olayı
+        self.ui.pushButton.clicked.connect(self.open_voice_show)
+    
+    def open_voice_show(self):
+        password = self.ui.lineEdit.text()  # Correctly reference lineEdit
+        if password == "enes123":
+            # VoiceShow penceresini aç
+            self.voice_window = VoiceShow()  # Create an instance of the VoiceShow window
+            self.voice_window.show()
+            self.close()  # Admin login penceresini kapat
+        else:
+            # Yanlış giriş mesajı göster
+            QtWidgets.QMessageBox.warning(self, "Hata", "Geçersiz şifre")
+
+class VoiceCommand3(QtWidgets.QMainWindow):  # Inherit from QMainWindow
+    def __init__(self, parent=None):
+        super(VoiceCommand3, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.open_admin_login)
+
+    def open_admin_login(self):
+        # Eğer AdminLogin penceresi zaten açıksa, tekrar açma
+        if not hasattr(self, 'admin_window') or not self.admin_window.isVisible():
+            self.admin_window = AdminLogin()  # AdminLogin penceresini aç
+            self.admin_window.show()
+            self.close()  # VoiceCommand3 penceresini kapat
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = VoiceCommand3()  # VoiceCommand3 penceresinden başlat
+    window.show()
+    sys.exit(app.exec_())
